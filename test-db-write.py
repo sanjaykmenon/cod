a41225c73e6d8def50c99f269420732e251f8ccb
Sanjay Krishna <3213319+sanjaykmenon@users.noreply.github.com>
2024-03-12 17:41:51 -0400

    this works!!

---
+++ test-db-write.py
@@ -0,0 +1,24 @@
+import lancedb
+import getpass
+from lancedb.embeddings import EmbeddingFunctionRegistry
+from lancedb.pydantic import LanceModel, Vector
+
+db = lancedb.connect("/tmp/db")
+registry = EmbeddingFunctionRegistry.get_instance()
+func = registry.get("openai").create(name="text-embedding-3-small")
+
+class Words(LanceModel):
+    text: str = func.SourceField()
+    vector: Vector(func.ndims()) = func.VectorField()
+
+table = db.create_table("words", schema=Words, mode ="overwrite")
+table.add(
+    [
+        {"text": "hello world"},
+        {"text": "goodbye world"}
+    ]
+    )
+
+query = "farewell"
+actual = table.search(query).limit(1).to_pydantic(Words)[0]
+print(actual.text)
