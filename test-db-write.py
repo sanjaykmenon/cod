f721f60d392177e33b62003269b5747508f2a1f3
Sanjay Krishna <3213319+sanjaykmenon@users.noreply.github.com>
2024-03-31 21:25:57 -0400

    new ideas

--- test-db-write.py
+++ test-db-write.py
@@ -11,13 +11,16 @@ class Words(LanceModel):
     text: str = func.SourceField()
     vector: Vector(func.ndims()) = func.VectorField()
 
-# table = db.create_table("words", schema=Words, mode ="overwrite")
-# table.add(
-#     [
-#         {"text": "hello world"},
-#         {"text": "goodbye world"}
-#     ]
-#     )
+table = db.create_table("words", schema=Words, mode ="overwrite")
+table.add(
+    [
+        {"text": "hello world"},
+        {"text": "goodbye world"}
+    ]
+    )
+
+#search pattern
+
 table = db.open_table("words")
 query = "hi"
 actual = table.search(query).limit(1).to_pydantic(Words)[0]
